f935a4bc357c3183b52b379284f9fca7a344e1e6
Milan R <github@milanramakers.nl>
2026-05-06 18:03:12 +0200

    Clear database connection credentials
    Removed sensitive database credentials from db.php.

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "217.154.164.56";
-$dbname = "s27_StreamHiveMilan";
-$username = "u27_FWbFmsXe9H";
-$password = "79hKgc4EcA5!H6@W14@NzGK9";
+$host = "";
+$dbname = "";
+$username = "";
+$password = "";
 
 try {
 
